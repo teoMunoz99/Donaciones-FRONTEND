7aaa71f2d326249aa5ba3738064940a92a7a2eed
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-08-10 14:24:05 -0300

    cambie el icono de vovler atras en el componente boton

--- src/components/common/bntVoler/BtnVolver.jsx
+++ src/components/common/bntVoler/BtnVolver.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./BtnVolver.css";
 import { NavLink } from "react-router-dom";
-import { MdArrowBackIosNew } from "react-icons/md";
+import { IoIosArrowBack } from "react-icons/io";
 
 const BtnVolver = () => {
   return (
     <div className="mt-4 mx-4">
       <NavLink to="/" className="text-decoration-none text-dark">
-        <MdArrowBackIosNew className="flecha" />
+        <IoIosArrowBack className="flecha" />
       </NavLink>
     </div>
   );
