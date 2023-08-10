ab51e09ce8a3dc21599a7fb40378d8e00b9a3385
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-08-10 18:34:42 -0300

    ajuste el css de la pantalla de bienvenida

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,24 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Welcome.css";
 import Options from "../views/Options";
+import { Container } from "react-bootstrap";
 
 const Welcome = () => {
   return (
     <div className="contenedor-inicio body">
-        <section className="d-flex flex-column align-items-center text-center contenedor-texto-img">
-          <figure id="bannerPrincipal">
-            <img
-              src="https://img.freepik.com/vector-gratis/personas-planas-mascotas-ilustradas_23-2148983750.jpg?t=st=1691449545~exp=1691450145~hmac=026b7308924df6cd095fb0ac907bff825f3c34ee3c1b028c7547c848e883e37b"
-              alt=""
-            />
-          </figure>
-          <h1 className="fw-bold text-light">Cambia vidas con un gesto, dona con el corazón</h1>
-          <p className="text-light tamanio-letra">
+      <Container className="d-flex flex-column align-items-center text-center contenedor-texto-img">
+        <figure id="bannerPrincipal">
+          <img
+            src="https://img.freepik.com/vector-gratis/personas-planas-mascotas-ilustradas_23-2148983750.jpg?t=st=1691449545~exp=1691450145~hmac=026b7308924df6cd095fb0ac907bff825f3c34ee3c1b028c7547c848e883e37b"
+            alt=""
+            className="img-fluid"
+          />
+        </figure>
+        <section className="contenedor-texto">
+          <h1 className="fw-bold text-light tamanio-titulo">
+            Cambia vidas con un gesto, dona con el corazón
+          </h1>
+          <p className="text-light tamanio-parrafo">
             Cada donación que realizas es un paso hacia un mundo mejor. Tu
             generosidad se convierte en esperanza, en sonrisas que se
             multiplican y en oportunidades que se hacen realidad.
           </p>
         </section>
+      </Container>
       <Options></Options>
     </div>
   );
--- src/components/views/Options.jsx
+++ src/components/views/Options.jsx
@@ -6,46 +6,49 @@ import { NavLink } from "react-router-dom";
 
 const Options = () => {
   return (
-    <div className="contenedor-opciones">
-      <h6 className="text-center m-3 fw-bold padding-titulo">Elegí una categoría</h6>
-      <Container>
-        <Row className="text-center px-3">
-          <Col className="btn-div">
+    <Container className="contenedor-opciones">
+      <h6 className="text-center m-3 fw-bold padding-titulo">
+        Elegí una categoría
+      </h6>
+        <Row className="row-cols-3">
+          <Col className="col">
             <NavLink to="*" className="text-decoration-none text-dark">
-              <figure className="figure">
+              <figure className="btn-tamanio">
                 <img
                   src="https://cdn2.iconfinder.com/data/icons/charity-37/512/Charity_37-256.png"
-                  alt="Medicamentos" className="img"
+                  alt="Medicamentos"
+                  className="img-fluid"
                 />
               </figure>
-              <p className="fw-semibold">Medicamentos</p>
+              <p className="fw-semibold text-center">Fármacos</p>
             </NavLink>
           </Col>
-          <Col className="btn-div">
+          <Col className="col">
             <NavLink to="*" className="text-decoration-none text-dark">
-              <figure className="figure">
+              <figure className="btn-tamanio">
                 <img
                   src="https://cdn2.iconfinder.com/data/icons/charity-37/512/Charity_6-512.png"
-                  alt="Alimentos" className="img"
+                  alt="Alimentos"
+                  className="img-fluid"
                 />
               </figure>
-              <p className="fw-semibold">Alimentos</p>
+              <p className="fw-semibold text-center">Alimentos</p>
             </NavLink>
           </Col>
-          <Col className="btn-div">
+          <Col className="col">
             <NavLink to="/Home" className="text-decoration-none text-dark">
-              <figure className="figure">
+              <figure className="btn-tamanio">
                 <img
                   src="https://cdn2.iconfinder.com/data/icons/charity-37/512/Charity_10-512.png"
-                  alt="Donar" className="img"
+                  alt="Donar"
+                  className="img-fluid"
                 />
               </figure>
-              <p className="fw-semibold">Donar</p>
+              <p className="fw-semibold text-center">Donar</p>
             </NavLink>
           </Col>
         </Row>
-      </Container>
-    </div>
+    </Container>
   );
 };
 
