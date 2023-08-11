f7e60c5992f552146473d10d8549d8c6d46b9a00
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-08-11 15:08:16 -0300

    agregue el componente detallesCausa

---
+++ src/components/pages/DetallesCausa.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./DetallesCausa.css";
+
+const DetallesCausa = () => {
+    return (
+        <div>
+            aaaa
+        </div>
+    );
+};
+
+export default DetallesCausa;
